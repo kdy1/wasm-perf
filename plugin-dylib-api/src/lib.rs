aef42c2e1c3e3ad1db3dc93c949b7a2902d2354c
Donny <kdy1997.dev@gmail.com>
2021-11-06 23:11:15 +0900

    Common

--- plugin-dylib-api/src/lib.rs
+++ plugin-dylib-api/src/lib.rs
@@ -1,8 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use abi_stable::{
+    library::RootModule,
+    package_version_strings,
+    sabi_types::VersionStrings,
+    std_types::{RResult, RStr, RString, RVec},
+    StableAbi,
+};
+
+/// Don't use this directly.
+#[repr(C)]
+#[derive(StableAbi)]
+#[sabi(kind(Prefix(prefix_ref = "SwcPluginRef")))]
+#[sabi(missing_field(panic))]
+pub struct SwcPlugin {
+    #[sabi(last_prefix_field)]
+    pub process_js:
+        Option<extern "C" fn(config_json: RStr, ast: RVec<u8>) -> RResult<RVec<u8>, RString>>,
+}
+
+impl RootModule for SwcPluginRef {
+    abi_stable::declare_root_module_statics! {SwcPluginRef}
+
+    const BASE_NAME: &'static str = "swc_plugin";
+    const NAME: &'static str = "swc_plugin";
+    const VERSION_STRINGS: VersionStrings = package_version_strings!();
 }
