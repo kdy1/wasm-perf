351186c9310f9f0add4d63735e344ffa6b0282d1
Donny <kdy1997.dev@gmail.com>
2021-11-06 23:05:40 +0900

    Impl

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,8 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use swc_common::{BytePos, Span};
+use swc_ecmascript::visit::{noop_fold_type, Fold};
+
+pub fn transform() -> impl Fold {
+    TestPlugin
+}
+
+struct TestPlugin;
+
+impl Fold for TestPlugin {
+    noop_fold_type!();
+
+    fn fold_span(&mut self, mut span: Span) -> Span {
+        span.lo = span.lo + BytePos(1);
+        span.hi = span.hi + BytePos(1);
+        span
     }
 }
