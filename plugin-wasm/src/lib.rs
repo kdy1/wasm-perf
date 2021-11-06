b93490e4e446cd5d656cf58a3f80d6aa4561f813
Donny <kdy1997.dev@gmail.com>
2021-11-06 23:30:59 +0900

    wasm plugin

--- plugin-wasm/src/lib.rs
+++ plugin-wasm/src/lib.rs
@@ -1,8 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use common::{deserialize_ast, serialize_ast};
+use swc_ecma_ast::Program;
+use swc_ecmascript::visit::FoldWith;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn process(ast: &[u8], conifg_json: &str) -> Vec<u8> {
+    let ast: Program = deserialize_ast(&ast).unwrap();
+
+    let config: plugin::Config = serde_json::from_str(conifg_json).unwrap();
+
+    let mut v = plugin::transform(config);
+
+    let ast = ast.fold_with(&mut v);
+
+    serialize_ast(&ast).unwrap()
 }
