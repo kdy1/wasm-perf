8326cffefe67e4e68e4e3cf2b7d25493af621c7e
Donny <kdy1997.dev@gmail.com>
2022-01-25 15:34:18 +0900

    Configure

--- plugin-wasm/src/lib.rs
+++ plugin-wasm/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate swc_node_base;
 
 use common::{deserialize_ast, serialize_ast};
+use swc_common::errors::{Handler, HANDLER};
 use swc_ecma_ast::Program;
 use swc_ecmascript::visit::FoldWith;
 use wasm_bindgen::prelude::*;
@@ -11,7 +12,8 @@ pub fn process(ast: &[u8], conifg_json: &str) -> Vec<u8> {
 
     let config: plugin::Config = serde_json::from_str(conifg_json).unwrap();
 
-    let mut v = plugin::transform(config);
+    let handler = Handler::with_emitter_writer(Box::new(vec![]), None);
+    let mut v = HANDLER.set(&handler, || plugin::transform(config));
 
     let ast = ast.fold_with(&mut v);
 
