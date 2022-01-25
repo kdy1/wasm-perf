041810fba6b4082f433b7425f4ccf33d85e2e7ee
Donny <kdy1997.dev@gmail.com>
2022-01-25 15:32:37 +0900

    Use witohut config

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use swc_common::{BytePos, Span};
+use swc_common::{errors::HANDLER, BytePos, Span};
 use swc_ecmascript::visit::{noop_fold_type, Fold};
 
 pub fn transform(_config: Config) -> impl Fold {
@@ -12,8 +12,13 @@ impl Fold for TestPlugin {
     noop_fold_type!();
 
     fn fold_span(&mut self, mut span: Span) -> Span {
+        HANDLER.with(|handler| {
+            handler.struct_span_err(span, "test").emit();
+        });
+
         span.lo = span.lo + BytePos(1);
         span.hi = span.hi + BytePos(1);
+
         span
     }
 }
